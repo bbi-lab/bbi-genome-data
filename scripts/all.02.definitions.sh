c8aac40d0906b0b10868650fbce15609074ee425
brgew <bge@uw.edu>
2022-01-06 17:50:49 -0800

    Reset STAGE_DIR

--- scripts/all.02.definitions.sh
+++ scripts/all.02.definitions.sh
@@ -8,8 +8,8 @@
 #
 # Staging directory.
 #
-# STAGE_DIR="/net/bbi/vol1/data/genomes_stage"
-STAGE_DIR="/net/bbi/vol1/data/bge/genomes/genomes_stage.test.20211229a"
+STAGE_DIR="/net/bbi/vol1/data/genomes_stage"
+# STAGE_DIR="/net/bbi/vol1/data/bge/genomes/genomes_stage.test.20211229a"
 
 
 #
